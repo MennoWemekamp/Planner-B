a8f818f6f9aa6f62a4fdeb6cb5f459fe6fbb4664
Menno Wemekamp <menno.wemekamp@gmail.com>
2023-09-28 16:26:34 +0200

    Add Tailwind and import fonts #20

---
+++ tailwind.config.cjs
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    colors: {
+      text: "var(--text)",
+      background: "var(--background)",
+      primary: "var(--primary)",
+      secondary: "var(--secondary)",
+      accent: "var(--accent)",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
